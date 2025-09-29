d30ebd42c470ec1914800112ab142de83216985d
COMPU-S035\daw2 <servellonrejasjuandavid@gmail.com>
2025-09-29 16:49:11 +0200

    avance carrito

---
+++ Carrito_compras/script.js
@@ -0,0 +1,31 @@
+class Carrito{
+    constructor(SKU, title, price){
+        this.SKU = SKU;
+        this.title = title;
+        this.price = price;
+    }
+}
+
+export const 
+    dato={
+        
+    currency: "€", 
+    products: [ 
+                { 
+                    SKU: "0K3QOSOV4V", 
+                    title: "iFhone 13 Pro", 
+                    price: "938.99" 
+                }, 
+                { 
+                    SKU: "TGD5XORY1L", 
+                    title: "Cargador", 
+                    price: "49.99" 
+                }, 
+                { 
+                    SKU: "IOKW9BQ9F3", 
+                    title: "Funda de piel", 
+                    price: "79.99" 
+                } 
+              ] 
+}
+    
