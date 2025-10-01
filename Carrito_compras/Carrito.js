cbcb45b4736767c9ae55e08ffc888c63d26d1ed6
COMPU-S035\daw2 <servellonrejasjuandavid@gmail.com>
2025-10-01 19:54:01 +0200

    avance carrito

---
+++ Carrito_compras/Carrito.js
@@ -0,0 +1,19 @@
+class Carrito{
+    constructor(SKU, title, price){
+        this.SKU = SKU;
+        this.title = title;
+        this.price = price;
+    }
+
+    actualizar(SKU, unit){
+
+    }
+
+    obtenerInformacionProducto(SKU){
+
+    }
+
+    obtenerCarrito(){
+
+    }
+}
--- Carrito_compras/script.js
+++ Carrito_compras/script.js
@@ -1,10 +1,21 @@
-class Carrito{
-    constructor(SKU, title, price){
-        this.SKU = SKU;
-        this.title = title;
-        this.price = price;
-    }
-}
+document.addEventListener('DOMContentLoaded', function(event) {
+
+const tabla = document.querySelector('.mitabla');
+const tbody = tabla.querySelector('tbody');
+const filas = tbody.querySelectorAll('tr');
+filas.forEach(fila => {
+  const celdas = fila.querySelectorAll('th, td');
+  const celdaCantidad = celdas[1];
+  console.log(celdaCantidad.textContent);
+});
+
+const btnMas = document.createElement('button');
+const btnMenos = document.createElement('button');
+
+btnMas.textContent = '+';
+btnMenos.textContent = '-'
+
+})
 
 export const 
     dato={
@@ -14,17 +25,17 @@ export const
                 { 
                     SKU: "0K3QOSOV4V", 
                     title: "iFhone 13 Pro", 
-                    price: "938.99" 
+                    price: "938.99",
                 }, 
                 { 
                     SKU: "TGD5XORY1L", 
                     title: "Cargador", 
-                    price: "49.99" 
+                    price: "49.99",
                 }, 
                 { 
                     SKU: "IOKW9BQ9F3", 
                     title: "Funda de piel", 
-                    price: "79.99" 
+                    price: "79.99",
                 } 
               ] 
 }
