485775f15caf7cd5d6ca269130f5f0e9fce3c21c
COMPU-S035\daw2 <servellonrejasjuandavid@gmail.com>
2025-11-27 18:33:18 +0100

    avance tienda productos

--- GestorDeProductos/src/app/pages/form/form.ts
+++ GestorDeProductos/src/app/pages/form/form.ts
@@ -1,11 +1,32 @@
+import { IProductos } from './../../interfaces/iproductos';
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.html',
   styleUrl: './form.css',
 })
 export class Form {
+
+  modelForm: FormGroup
+  private sku: number = 0;
+
+  constructor(){
+    this.modelForm = new FormGroup({
+      // sku: new FormGroup (-1,),
+      // title: new FormGroup (null,[Validators.required]),
+      // description: new FormGroup (null,[Validators.required]),
+      // price: new FormGroup (null, [Validators.required]),
+      // url: new FormGroup (null, [Validators.required])
+    },[])
+  }
+
+  getDataForm() {
+
+    
+  }
 
 }
