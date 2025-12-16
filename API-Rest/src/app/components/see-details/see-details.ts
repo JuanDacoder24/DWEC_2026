8da6508fc6349e2c3f49a4c7afacfc95a438120a
JuanDacoder24 <servellonrejasjuandavid@gmail.com>
2025-12-17 00:30:32 +0100

    CONTINUAR CON EL FORMULARIO

---
+++ API-Rest/src/app/components/see-details/see-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-see-details',
+  imports: [],
+  templateUrl: './see-details.html',
+  styleUrl: './see-details.css',
+})
+export class SeeDetails {
+
+}
--- API-Rest/src/app/pages/form/form.ts
+++ API-Rest/src/app/pages/form/form.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ServicioUsuario } from '../../services/servicio-usuario';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -7,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './form.css',
 })
 export class Form {
+
+  modelForm: FormGroup
+  private id: number
+
+  servicioUsuario = inject(ServicioUsuario)
+  activedRoute = inject(ActivatedRoute)
+  router = inject(Router)
+
+  constructor(){
+    this.id = 0
+    this.modelForm = new FormGroup({
+      first_name: new FormControl(null, [Validators.required]),
+      last_name: new FormControl(null,[Validators.required]),
+      username: new FormControl(null,[Validators.required]),
+      email: new FormControl(null,[Validators.required]),
+      image: new FormControl(null,[Validators.required]),
+      password: new FormControl(null,[Validators.required]),
+    })
+  }
+
+  checkControl(FormControlName: string, validator: string): boolean | undefined{
+    return this.modelForm.get(FormControlName)?.hasError(validator) && this.modelForm.get(FormControlName)?.touched;
+  }
 
 }
