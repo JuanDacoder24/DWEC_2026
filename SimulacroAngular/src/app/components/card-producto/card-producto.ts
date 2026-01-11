083cd3b2170c353b2c0eedfa655f6cde824653c0
JuanDacoder24 <servellonrejasjuandavid@gmail.com>
2026-01-11 19:57:37 +0100

    no message

---
+++ SimulacroAngular/src/app/components/card-producto/card-producto.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-producto',
+  imports: [],
+  templateUrl: './card-producto.html',
+  styleUrl: './card-producto.css',
+})
+export class CardProducto {
+
+}
