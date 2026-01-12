d0c85161d63914941d1c3504ec235652726356c2
COMPU-S035\daw2 <servellonrejasjuandavid@gmail.com>
2026-01-12 16:03:35 +0100

    terminar de hacer el log in y demas, primero verificar los cambios

--- SimulacroAngular/src/app/app.routes.ts
+++ SimulacroAngular/src/app/app.routes.ts
@@ -2,11 +2,22 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Form } from './pages/form/form';
 import { ProductList } from './pages/product-list/product-list';
+import { UserList } from './pages/user-list/user-list';
+import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
     {path: '', pathMatch: 'full', redirectTo: 'home'},
     {path: 'home', component: Home},
+    {path: 'login', component: Login},
     {path: 'form', component: Form},
-    {path: 'productList', component: ProductList},
+    {
+        path: 'dashboard', component: Dashboard, children:
+        [
+            {path: '', pathMatch: 'full', redirectTo: 'productList'},
+            {path: 'productList', component: ProductList},
+            {path: 'userList', component: UserList},
+        ]
+    },
     {path: '**', redirectTo: 'home'},
 ];
